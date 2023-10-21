0874c80cd0ff9a1659a28c58a26df02657938a18
frangarcia8 <fjgarlop@gmail.com>
2023-10-22 00:54:09 +0200

    Update Ejercicio1.sql

--- Ejercicio1.sql
+++ Ejercicio1.sql
@@ -1,8 +1,16 @@
 /*1. Crear una tabla llamada "Clientes" con las columnas: id (entero, clave primaria), nombre (texto) y email (texto). */
 
-CREATE TABLE clientes(
+CREATE TABLE IF NOT EXISTS clientes(
 id SERIAL PRIMARY KEY,
 nombre VARCHAR(255) NOT NULL,
 email VARCHAR(255)
 )
+
+/*Insertar un nuevo cliente en la tabla "Clientes" con id=1, nombre="Juan" y
+email="juan@example.com".*/
+
+INSERT INTO public.clientes(nombre, email)
+VALUES ('Juan', 'juan@example.com')
+
+/*3. Actualizar el email del cliente con id=1 a "juan@gmail.com".*/
 
